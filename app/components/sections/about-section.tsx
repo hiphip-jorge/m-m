70afe1964b75a856247a2e815a0cb851399e3a3d
Jorge Perez <jorgeperez@MacBook-Air.local>
2023-04-17 23:12:36 -0500

    refactor: about me to about section

--- app/components/sections/about-section.tsx
+++ app/components/sections/about-section.tsx
@@ -1,16 +1,31 @@
-type Props = {
-  children: React.ReactNode;
-};
-
-const AboutMe = (props: Props) => {
+const AboutSection = () => {
   return (
     <section
-      id="aboutMe"
+      id="AboutSection"
       className="section-container flex h-screen flex-col justify-between"
     >
-      {props.children}
+      <div className="flex h-[350px] whitespace-nowrap">
+        <div className="section-header z-10 h-fit">
+          <h1 className="mb-2 w-fit bg-white pr-4 dark:bg-[#222]">Hi,</h1>
+          <h1 className="mb-2 w-fit bg-white pr-4 dark:bg-[#222]">I'm Tia</h1>
+          <h1 className="mb-2 w-fit bg-white pr-4 dark:bg-[#222]">Trini</h1>
+        </div>
+
+        <div className="">
+          <span className="absolute right-0 flex h-[350px] w-[306px] items-center justify-center bg-black text-white">
+            Photo
+          </span>
+        </div>
+      </div>
+      <article>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ullam
+          nemo, laborum ea hic voluptatem libero optio! laborum ea hic
+          voluptatem libero optio!
+        </p>
+      </article>{" "}
     </section>
   );
 };
 
-export default AboutMe;
+export default AboutSection;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,6 +8,7 @@ import { gallery_icon } from "~/utils.svg";
 import { useOutletContext } from "@remix-run/react";
 import { ContextType } from "~/root";
 import HeroSection from "~/components/sections/hero-section";
+import AboutSection from "~/components/sections/about-section";
 
 export default function Index() {
   let { isDarkMode, theme, isMenuOpen } = useOutletContext<ContextType>();
@@ -15,28 +16,7 @@ export default function Index() {
   return (
     <main className="min-h-screen sm:flex sm:items-center sm:justify-center">
       <HeroSection isDarkMode={isDarkMode} isMenuOpen={isMenuOpen} />
-      <AboutMe>
-        <div className="flex h-[350px] whitespace-nowrap">
-          <div className="section-header z-10 h-fit">
-            <h1 className="mb-2 w-fit bg-white pr-4 dark:bg-[#222]">Hi,</h1>
-            <h1 className="mb-2 w-fit bg-white pr-4 dark:bg-[#222]">I'm Tia</h1>
-            <h1 className="mb-2 w-fit bg-white pr-4 dark:bg-[#222]">Trini</h1>
-          </div>
-
-          <div className="">
-            <span className="absolute right-0 flex h-[350px] w-[306px] items-center justify-center bg-black text-white">
-              Photo
-            </span>
-          </div>
-        </div>
-        <article>
-          <p className="text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            ullam nemo, laborum ea hic voluptatem libero optio! laborum ea hic
-            voluptatem libero optio!
-          </p>
-        </article>
-      </AboutMe>
+      <AboutSection />
       <Testimonials isDarkMode={isDarkMode}>
         <h1 className="section-header">Testimonials</h1>
       </Testimonials>
