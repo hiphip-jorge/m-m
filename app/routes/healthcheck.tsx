4a9c84aaf8ddd26d1e69c2ddba3b927a1eaed950
Jorge Perez <jorgeperez@MacBook-Air.local>
2023-07-25 20:50:54 -0500

    refactor: remove data/prisma

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,8 +1,6 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderArgs } from "@remix-run/node";
 
-import { prisma } from "~/db.server";
-
 export async function loader({ request }: LoaderArgs) {
   const host =
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
